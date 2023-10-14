d6e508b7a245c972f3ec024b14c5dd7e19dc86a2
dbstjdghks03 <snu15119@snu.ac.kr>
2023-10-15 05:34:59 +0900

    requirement 2: Table 'User', 'Seller', 'Seller_User' requirements에 맞게 모든 column 이름들은 csv 파일과 동일하게 하였음. seller_user의 경우 user_id와 seller_id가 1:n의 관계이기 때문에 SELLER_USER_ID를 새롭게 만들었음.

--- DMA_project1_team07.py
+++ DMA_project1_team07.py
@@ -28,12 +28,14 @@ def requirement2(host, user, password):
 
     # TODO: WRITE CODE HERE
     cursor.execute("USE DMA_team07")
-    cursor.execute("DROP TABLE USER")
+
+    #USER
+    #cursor.execute("DROP TABLE user ;")
     cursor.execute("""
-    CREATE TABLE IF NOT EXISTS USER(
-    Uid INT NOT NULL,
-    Unickname VARCHAR(20), 
-    PRIMARY KEY (Uid)) ;               
+    CREATE TABLE IF NOT EXISTS user(
+    id INT(11) NOT NULL,
+    nickname VARCHAR(255), 
+    PRIMARY KEY (id)) ;               
                    """)
     filepath = '/Users/yunseonghwan/Desktop/2023-2/데이터 관리와 분석/DMA_project1/Project1_dataset'+'/'+'user.csv'
 
@@ -53,9 +55,99 @@ def requirement2(host, user, password):
 
             data_to_insert.append(tuple(formatted_row))
 
-    insert_query = "INSERT INTO USER (Uid, Unickname) VALUES (%s, %s)"
+    insert_query = "INSERT INTO user (id, nickname) VALUES (%s, %s) ;"
     cursor.executemany(insert_query, data_to_insert)
     cnx.commit()
+
+    #SELLER
+    #cursor.execute("DROP TABLE seller")
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS seller(
+    id VARCHAR(255) NOT NULL,
+    Name VARCHAR(255),
+    address VARCHAR(255),
+    PRIMARY KEY (id)) ;               
+    """)
+    filepath = '/Users/yunseonghwan/Desktop/2023-2/데이터 관리와 분석/DMA_project1/Project1_dataset'+'/'+'seller.csv'
+
+    with open(filepath, 'r', encoding='utf-8') as csv_data:
+        data_to_insert = []
+        for row in csv_data.readlines()[1:]:
+            row = row.strip().split(',')
+            formatted_row = []
+
+            for idx, data in enumerate(row):
+                if data == '':
+                    formatted_row.append(None)
+                else:
+                    formatted_row.append(data)
+
+            data_to_insert.append(tuple(formatted_row))
+
+    insert_query = "INSERT INTO seller (id, Name, address) VALUES (%s, %s, %s) ;"
+    cursor.executemany(insert_query, data_to_insert)
+    cnx.commit()
+
+    #SELLER_USER_ID
+    #cursor.execute("DROP TABLE SELLER_USER_ID ;")
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS SELLER_USER_ID (
+    SUIid INT(11) NOT NULL,
+    PRIMARY KEY (SUIid)) ;
+    """)
+
+    filepath = '/Users/yunseonghwan/Desktop/2023-2/데이터 관리와 분석/DMA_project1/Project1_dataset' + '/' + 'seller_user.csv'
+
+    with open(filepath, 'r', encoding='utf-8') as csv_data:
+        data_to_insert = set()
+        for row in csv_data.readlines()[1:]:
+            row = row.strip().split(',')
+            formatted_row = []
+
+            for idx, data in enumerate(row):
+                if data == '':
+                    formatted_row.append(None)
+                if idx in [0]:
+                    if int(data) in data_to_insert:
+                        pass
+                    formatted_row.append(int(data))
+
+            data_to_insert.add(tuple(formatted_row))
+
+    data_to_insert = list(data_to_insert)
+    insert_query = "INSERT INTO SELLER_USER_ID (SUIid) VALUES (%s) ;"
+    cursor.executemany(insert_query, data_to_insert)
+    cnx.commit()
+
+    # SELLER_USER
+    #cursor.execute("DROP TABLE SELLER_USER ;")
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS seller_user(
+        user_id INT(11) NOT NULL,
+        seller_id VARCHAR(255) NOT NULL) ;               
+        """)
+    filepath = '/Users/yunseonghwan/Desktop/2023-2/데이터 관리와 분석/DMA_project1/Project1_dataset' + '/' + 'seller_user.csv'
+
+    with open(filepath, 'r', encoding='utf-8') as csv_data:
+        data_to_insert = []
+        for row in csv_data.readlines()[1:]:
+            row = row.strip().split(',')
+            formatted_row = []
+
+            for idx, data in enumerate(row):
+                if data == '':
+                    formatted_row.append(None)
+                elif idx in [0]:
+                    formatted_row.append(int(data))
+                else:
+                    formatted_row.append(data)
+
+            data_to_insert.append(tuple(formatted_row))
+
+    insert_query = "INSERT INTO seller_user (user_id, seller_id) VALUES (%s, %s) ;"
+    cursor.executemany(insert_query, data_to_insert)
+    cnx.commit()
+
 
     print("Data inserted successfully!")
 
